de006ae2e5a64bbaba42c0cfcdfffabfe3b48803
SamPatt <git@sampatt.com>
2023-12-15 13:24:26 -0500

    create endpoint loads templates images

--- controllers/templates.js
+++ controllers/templates.js
@@ -1,27 +1,30 @@
+const axios = require('axios');
 
 const index = async (req, res, next) => {
   console.log('received index request');
   try {
-    // api
-    res.send('send back template images')
-    // res.status(200).json(images);
+    const response = await axios.get('https://api.imgflip.com/get_memes');
+    const memes = response.data.data.memes; // Extract the memes array from the response
+    res.status(200).json(memes); // Send back the memes array
   } catch (err) {
+    console.error(err);
     res.status(400).json({ err: err.message });
   }
 };
 
-const search = async (req, res, next) => {
-    console.log('received index request');
-    try {
-      // api
-      res.send('send back template images from a search')
-      // res.status(200).json(images);
-    } catch (err) {
-      res.status(400).json({ err: err.message });
-    }
-  };
-
 module.exports = {
-  index,
-  search
-}
+  index
+};
+
+
+
+// const search = async (req, res, next) => {
+//     console.log('received index request');
+//     try {
+//       // api
+//       res.send('send back template images from a search')
+//       // res.status(200).json(images);
+//     } catch (err) {
+//       res.status(400).json({ err: err.message });
+//     }
+//   };
--- routes/templates.js
+++ routes/templates.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const templatesCtrl = require('../controllers/templates')
 
 router.get("/", templatesCtrl.index);
-router.get("/:search", templatesCtrl.search);
+// router.get("/:search", templatesCtrl.search);
 
 
 module.exports = router
